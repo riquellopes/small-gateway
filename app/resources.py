c194568bbee39a6de87c3ee441071115769d204c
Henrique Lopes <riquellopes@gmail.com>
2018-08-05 09:19:20 -0300

    removing the swagger information of the python code

--- app/resources.py
+++ app/resources.py
@@ -4,6 +4,7 @@ import http
 from flask import jsonify, make_response
 from flask_restful import Resource
 from webargs.flaskparser import use_args
+from flasgger import swag_from
 
 from app.schemas import PaymentCreditCardSchema, PaymentBoletoSchema
 from app.db import db
@@ -14,69 +15,8 @@ class PaymentResource(Resource):
     @use_args(
         PaymentCreditCardSchema(
             strict=True, only=("amount", "buyer", "credit_card", "X-CLIENT")), locations=("headers", "json"))
+    @swag_from('docs/credit_card.yml')
     def post(self, payment):
-        """
-       Captura o pagamento do comprador.
-       ---
-       parameters:
-         - in: header
-           name: X-CLIENT
-           schema:
-             type: string
-           required: true
-         - in: body
-           name: data
-           required: true
-           schema:
-             id: Payload
-             required:
-               - amount
-             properties:
-               amount:
-                  type: string
-               buyer:
-                  type: object
-                  properties:
-                    name:
-                      type: string
-                    email:
-                      type: string
-                    cpf:
-                      type: string
-               creditcard:
-                  type: object
-                  properties:
-                    holder_name:
-                      type: string
-                    number:
-                      type: string
-                    expiration_date:
-                       type: string
-                    cvv:
-                       type: string
-       responses:
-         200:
-           description: Pagamento realizado com sucesso.
-           schema:
-             type: object
-         403:
-           description: Cliente inválido.
-           schema:
-             type: object
-             properties:
-               errors:
-                 type: object
-                 properties:
-                    client:
-                      type: array
-                      items:
-                        type: string
-         422:
-           description: Houve algum erro ao processar a requisição
-           schema:
-             type: object
-        """
-
         db.session.add(payment)
         db.session.commit()
 
