71e604fa393b8b6a10616f00b633a30a8d362012
Henrique Lopes <riquellopes@gmail.com>
2018-08-07 22:17:11 -0300

    refactoring code

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,7 +2,7 @@ from .factories import CreditCardPaymentFactory, BoletoPaymentFactory
 
 
 def test_when_boleto_payment_code_get_the_boleto_number(boleto):
-    pay = BoletoPaymentFactory.create(
+    pay = BoletoPaymentFactory(
         type=boleto,
 
         id=1,
@@ -12,10 +12,8 @@ def test_when_boleto_payment_code_get_the_boleto_number(boleto):
 
 
 def test_when_creditcard_payment_code_get_is_none(credit_card):
-    pay = CreditCardPaymentFactory.create(
-        type=credit_card,
-
-        id=2,
+    pay = CreditCardPaymentFactory(
+        type=credit_card
     )
 
     assert pay.code is None
